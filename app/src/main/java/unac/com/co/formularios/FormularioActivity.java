2cd4236bd6fb3dee717514657a271b95d5dc5268
Antonio Miguel <antonnio762@gmail.com>
2018-08-12 08:50:33 -0500

    Initial commit

---
+++ app/src/main/java/unac/com/co/formularios/FormularioActivity.java
@@ -0,0 +1,13 @@
+package unac.com.co.formularios;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.formulario);
+    }
+}
